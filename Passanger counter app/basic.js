22285c52b78916c6173537473d31508b6a9cb1dc
Kalyani Bajirao <111046479+Kalyani210@users.noreply.github.com>
2022-08-18 17:23:10 +0530

    Add files via upload

---
+++ Passanger counter app/basic.js
@@ -0,0 +1,137 @@
+//console.log("Hello World");
+
+//count = 5
+//console.log(count);
+
+//let firstbatch =5
+//let secondbatch =7
+//let count = firstbatch + secondbatch
+//console.log(count);
+
+
+//create variable, myage, and set its value to your age, log the myage variable to the console.
+//myAge=22
+//console.log(myAge);
+
+
+//1.create two variables myage and humandog ratio
+//2.multiply two together the two together and store results in mydog
+//3.log mydog to the console
+
+//let myAge = 22
+//let HumanDogRatio = 7
+//let myDogAge = myAge + HumanDogRatio
+//console.log(myDogAge);
+
+//create a variable, bonusPoints, Initialize it as 50. Increase it to 100.
+//Decrease it down to 25, and then finally increased it to 70.
+//console.log the value after each step.
+
+//let bonusPoints = 50
+//console.log(bonusPoints);
+
+//bonusPoints = bonusPoints + 50
+//console.log(bonusPoints);
+
+//bonusPoints = bonusPoints -75
+//console.log(bonusPoints);
+
+//bonusPoints = bonusPoints + 45
+//console.log(bonusPoints);
+
+
+// setting up the the race
+//Functions in JS
+
+function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+
+countdown()
+
+//Go
+//Players are running the race
+//Race is finished
+
+//Get ready for new race
+
+//create a function (You decide the name) that logs out the number 42 to the console
+//Call/invoke the function
+
+function multiplication() {
+    console.log(3*2*7)
+}
+multiplication()
+
+function myLogger() {
+    console.log(42)
+}
+myLogger()
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+// Create a funcyion that logs out the sun of all the lap times
+function logLapTime() {
+    let totalTime = lap1 + lap2 + lap3
+    console.log(totalTime)
+}
+
+logLapTime()
+
+let lapsCompleted = 0
+
+//Create a funnction that increments the lapCompleted variable with one
+//Run it three times
+function incrementLap() {
+    lapsCompleted = lapsCompleted + 1
+}
+
+incrementLap()
+incrementLap()
+incrementLap()
+
+console.log(lapsCompleted)
+
+//Strings in JS
+
+let username = "per"
+console.log(username);
+
+//Create a variable, message, that stores the string : "You have three new notifications"
+
+Message = "You have three new notifications"
+console.log(Message + "," + username + "!");
+
+//create a variable , messageToUser, that contains the message wee have logged
+let messageToUser = Message + "," + username + "!"
+console.log(messageToUser);
+
+//create two variables, name and greeting. The name variable should store your name,
+// and the greeting should store e.g. "hi, my name is"
+
+Name = "Kalyani"
+Greeting = "Hi, My name is"
+
+//create a third variable , myGreeting, that concentrates the two strings
+//log myGreeting to the console
+
+myGreeting = Greeting + " " + Name +"!"
+console.log(myGreeting)
+
+//String vs Numbers
+Points = 4
+BonusPoints = "10"
+TotalPoints = Points + BonusPoints
+console.log(TotalPoints)
+
+console.log(4+5)//9
+console.log("2"+"4") //"24"
+console.log("5"+1) //"51"
+console.log(100+"100") // 100100
+
