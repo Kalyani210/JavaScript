015025399b4c1c06b0506c659fe1fd82b9123774
Kalyani Bajirao <111046479+Kalyani210@users.noreply.github.com>
2022-08-18 20:20:37 +0530

    Add files via upload

---
+++ e-commerce website/index.js
@@ -0,0 +1,13 @@
+// when the user clicks the purchase button, render out
+//"Something went wrong, please try again" in the background
+// that has id="error".
+
+let errorParagraph = document.getElementById("error")
+console.log(errorParagraph)
+
+
+
+function purchase() {
+    console.log("button clicked")
+    errorParagraph.textContent = "Something went wrong, please try again"
+}
